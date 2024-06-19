172ed2ab5df2a85d6fac47274d99449fc113c398
ShronikArora <106712728+ShronikArora@users.noreply.github.com>
2024-06-19 14:10:15 -0400

    Update models.py
    The models for user and category need work which will be done later as the website starts to develop

--- models.py
+++ models.py
@@ -1,1 +1,93 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+from django.conf import settings
+
+# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+class Adress(models.Model):
+    adress_line = models.CharField(max_length= 400)
+    city= models.CharField(max_lenght=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.address_line1}, {self.city}, {self.state}, {self.country}"
+
+class Category(models.Model):
+    name = models.CharField(max_lenght= 100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    brand = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_update=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='attributes')
+    attribute_name = models.CharField(max_length=100)
+    attribute_value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.attribute_name}: {self.attribute_value}"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.image_url
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.email}"
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.DecimalField(decimal_places=2)
+    description = models.TextField()
+    order= models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Review by {self.user.email} for {self.product.title}"
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, unique= True )
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.title} "
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.title} in order {self.order.id}"
 
